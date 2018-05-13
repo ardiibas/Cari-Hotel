f736dfb6519ed9986d29da1ebde912758bf75783
ardiibas <mistersemar.ab@gmail.com>
2018-05-13 15:55:26 +0700

    add menu toolbar, add about activity

--- app/src/main/java/id/gifood/carihotel/view/MainActivity.java
+++ app/src/main/java/id/gifood/carihotel/view/MainActivity.java
@@ -1,5 +1,6 @@
 package id.gifood.carihotel.view;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -39,6 +41,22 @@ public class MainActivity extends AppCompatActivity implements
 
         bottomNavigationMenu = findViewById(R.id.main_bottom_menu);
         bottomNavigationMenu.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_about:
+                startActivity(new Intent(this, About.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
